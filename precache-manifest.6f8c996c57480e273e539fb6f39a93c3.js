df4aae6913ea8309f79b5f153641d7bedd289241
wickdeeb <isak.m.h.s@gmail.com>
2019-05-03 15:03:35 +0200

    Updates

---
+++ precache-manifest.6f8c996c57480e273e539fb6f39a93c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4d3cb8c8c281d5cd90f",
+    "url": "/frontend-realestate/static/js/runtime~main.d93a21c2.js"
+  },
+  {
+    "revision": "721762e3f2cd41e32e3c",
+    "url": "/frontend-realestate/static/js/main.d34ba447.chunk.js"
+  },
+  {
+    "revision": "d767a8e8c4604ee9d108",
+    "url": "/frontend-realestate/static/js/2.cfc1ff55.chunk.js"
+  },
+  {
+    "revision": "721762e3f2cd41e32e3c",
+    "url": "/frontend-realestate/static/css/main.3f007843.chunk.css"
+  },
+  {
+    "revision": "42efb6bf1b50a04eea2aca69e220caab",
+    "url": "/frontend-realestate/index.html"
+  }
+];
